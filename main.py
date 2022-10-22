ca4f4a79494b5173567b8535d3a90d01aeca3c45
fmata97 <fabiomata97@gmail.com>
2022-10-22 16:54:26 +0100

    Added input validation when asking user if he wants to keep playing

--- main.py
+++ main.py
@@ -5,12 +5,21 @@ from SlotMachine import *
 # the game itself
 
 slotMachine = SlotMachine()
+end = False
 
 while True:
+    if end:
+        break
+        
     slotMachine.play()
     if slotMachine.balance == 0:
         print("Game over! No credit$ left...")
         break
-    answer = input("Do you want to keep playing? (y/n)")
-    if answer == "n":
-        break
+    while True:
+        answer = input("Do you want to keep playing? (y/n)")
+        if answer == "n":
+            end = True
+            print("\nUntil next time!")
+            break
+        elif answer == "y":
+            break
